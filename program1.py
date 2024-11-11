241db4eafbba26bfe846d3bc298545db3bf619d2
Saiteja <saitejakorra2002@gmail.com>
2024-11-11 13:29:17 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -8,6 +8,10 @@ class Solution:
             if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 'W':
                 return 
             grid[r][c] = 'W'
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
             
                   
         return 0
