089bb6a131179634827f621552d4609905e54ebd
Saiteja <saitejakorra2002@gmail.com>
2024-11-11 13:28:00 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,5 +4,9 @@ class Solution:
     #    write your code here
         if not grid:
             return 0
+        def dfs(r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r]grid[c] == 'W':
+                return 
+                
                   
         return 0
