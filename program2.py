e136b11d03eeb090d218a4c038971741144eae92
Saiteja <saitejakorra2002@gmail.com>
2024-11-11 13:58:21 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,20 +1,4 @@
-# def decode_message( s: str, p: str) -> bool:
-#         dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
-#         dp[0][0] = True  
-#         for j in range(1, len(p) + 1):
-#                 if p[j - 1] == '*':
-#                         dp[0][j] = dp[0][j - 1]
-        
-#         for i in range(1, len(s) + 1):
-#                 for j in range(1, len(p) + 1):
-#                         if p[j - 1] == '*':
-#                                 dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
-#                         elif p[j - 1] == '?':
-#                                 dp[i][j] = dp[i - 1][j - 1]
-#                         else:
-#                                 dp[i][j] = dp[i - 1][j - 1] and s[i - 1] == p[j - 1]
-        
-#         return dp[len(s)][len(p)]
+
 # write your code here
 
 def decode_message(s: str, p: str) -> bool:
